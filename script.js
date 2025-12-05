1104f846646dc5ce66c93a82e4c91a6735e545ba
mehdi el-hajjame <el-hajjame@outlook.com>
2025-12-05 15:17:29 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,41 @@
+'use strict';
+
+const questions = [{
+    question : 'here first question?',
+    answer:[
+        {text : 'answer one', correct: false},
+        {text : 'answer two', correct: true},
+        {text : 'answer two', correct: false},
+        {text : 'answer two', correct: false},
+    ]
+},
+{
+    question : 'here first question?',
+    answer:[
+        {text : 'answer one', correct: false},
+        {text : 'answer two', correct: true},
+        {text : 'answer two', correct: false},
+        {text : 'answer two', correct: false},
+    ]
+},
+
+{
+    question : 'here first question?',
+    answer:[
+        {text : 'answer one', correct: false},
+        {text : 'answer two', correct: true},
+        {text : 'answer two', correct: false},
+        {text : 'answer two', correct: false},
+    ]
+},
+
+{
+    question : 'here first question?',
+    answer:[
+        {text : 'answer one', correct: false},
+        {text : 'answer two', correct: true},
+        {text : 'answer two', correct: false},
+        {text : 'answer two', correct: false},
+    ]
+},
+]
